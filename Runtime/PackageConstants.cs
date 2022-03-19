5bb3d49e6922f239b22fa33558f1bef9b1660a39
Walter Hulsebos <walterhulsebos@outlook.com>
2022-03-19 20:13:19 +0100

    NEW (Script) Terminal.Logging
    Total replacement for Debug.Log

---
+++ Runtime/PackageConstants.cs
@@ -0,0 +1,9 @@
+using System.Runtime.CompilerServices;
+
+namespace CGTK.Utils.Terminal
+{
+    public static class PackageConstants
+    {
+        internal const MethodImplOptions INLINE = MethodImplOptions.AggressiveInlining;        
+    }
+}
